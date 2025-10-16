07912ab6c82adae69eba6d332286ed9fd6e3ba47
koneko <67551503+koneko@users.noreply.github.com>
2025-10-17 01:18:25 +0200

    i hate this stupid shit

--- web/auth.config.ts
+++ web/auth.config.ts
@@ -5,8 +5,12 @@ import { defineConfig } from "auth-astro";
 export default defineConfig({
     providers: [
         Discord({
-            clientId: import.meta.env.DDMT_WEB_CLIENT_ID,
-            clientSecret: import.meta.env.DDMT_WEB_CLIENT_SECRET,
+            clientId:
+                import.meta.env.DDMT_WEB_CLIENT_ID ||
+                process.env.DDMT_WEB_CLIENT_ID,
+            clientSecret:
+                import.meta.env.DDMT_WEB_CLIENT_SECRET ||
+                process.env.DDMT_WEB_CLIENT_SECRET,
         }),
     ],
     secret: import.meta.env.AUTH_SECRET || process.env.AUTH_SECRET,
